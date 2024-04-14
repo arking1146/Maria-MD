f2fcd56320637593978e5f3e48f6deafe46e1fd4
arking1146 <166977481+arking1146@users.noreply.github.com>
2024-04-14 16:58:51 +0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,9 +2,9 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
+global.ownernumber = ['+923137811619']
 global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
+global.ytname = "YT: professor.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
 
